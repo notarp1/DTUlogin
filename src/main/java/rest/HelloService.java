a5a9448f02ee66f551101c02079798dbab535eb1
christian <ChristiaNotarmaso@gmail.com>
2020-05-07 16:26:23 +0200

    Yeet

---
+++ src/main/java/rest/HelloService.java
@@ -0,0 +1,14 @@
+package rest;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("hello")
+public class HelloService {
+
+    @GET
+    public String getHello(){
+        return "hello";
+    }
+}
